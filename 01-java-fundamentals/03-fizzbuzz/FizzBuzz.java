266e6a9afb075820c7967debe4b470af2a0ace3c
Chris <cmsl4y3r@gmail.com>
2021-08-28 15:48:59 -0600

    fizzbuzz assignment completed, overloaded for YeeHaw

--- 01-java-fundamentals/03-fizzbuzz/FizzBuzz.java
+++ 01-java-fundamentals/03-fizzbuzz/FizzBuzz.java
@@ -1,5 +1,34 @@
 public class FizzBuzz {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public String fizzBuzz(int number) {
+        String answer;
+        if(number % 3 == 0  && number % 5 == 0){
+            answer = "FizzBuzz";
+        }else if(number % 5 == 0) {
+            answer = "Buzz";
+        }else if(number % 3 == 0) {
+            answer = "Fizz";
+        }else {
+            answer = String.valueOf(number);
+        }
+    return answer;
     }
+
+    public String fizzBuzz(
+        int number, 
+        String multOf3Word, 
+        String multOf5Word, 
+        String multOf15Word
+        ) {
+            String answer;
+            if(number % 3 == 0 && number % 5 == 0) {
+                answer = multOf15Word;
+            }else if(number % 5 == 0){
+                answer = multOf5Word;
+            }else if(number % 3 == 0) {
+                answer = multOf3Word;
+            }else {
+                answer = String.valueOf(number);
+            }
+            return answer;
+        }
 }
---
+++ 01-java-fundamentals/03-fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,21 @@
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzzTest = new FizzBuzz();
+
+        String testFizzBuzz = fizzBuzzTest.fizzBuzz(3);
+        String secondFizzBuzz = fizzBuzzTest.fizzBuzz(5);
+        String thirdFizzBuzz = fizzBuzzTest.fizzBuzz(15);
+        String fourthFizzBuzz = fizzBuzzTest.fizzBuzz(7);
+        String overloadedFizzBuzz = fizzBuzzTest.fizzBuzz(15, "Yee", "Haw", "YeeHaw");
+        
+        System.out.println(testFizzBuzz);
+        System.out.println(secondFizzBuzz);
+        System.out.println(thirdFizzBuzz);
+        System.out.println(fourthFizzBuzz);
+        System.out.println(overloadedFizzBuzz);
+
+        for(int i = 1; i <= 50; i++) {
+            System.out.println(fizzBuzzTest.fizzBuzz(i, "Yee", "Haw", "YeeHaw"));
+        }
+    }
+}
