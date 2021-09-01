e369590997d2b5c933877cde3f768318dda723c4
Chris <cmsl4y3r@gmail.com>
2021-08-31 20:43:18 -0600

    Completed all items except the sensei bonus so far

--- 01-java-fundamentals/06-puzzling/PuzzleJavaTest.java
+++ 01-java-fundamentals/06-puzzling/PuzzleJavaTest.java
@@ -5,8 +5,17 @@ public class PuzzleJavaTest {
         PuzzleJava puzzleTester = new PuzzleJava();
 
         // System.out.println(puzzleTester.make10Random());
-        System.out.println(puzzleTester.charGenerator());
+        // System.out.println(puzzleTester.charGenerator());
 
+        // System.out.println(puzzleTester.generatePassword());
+        
+        ArrayList<String> newSet = puzzleTester.getNewPasswordSet(10);
+
+        
+        for(int i=0; i<newSet.size(); i++){
+            System.out.println(newSet.get(i));
+        }
+        
 
     }
 }
