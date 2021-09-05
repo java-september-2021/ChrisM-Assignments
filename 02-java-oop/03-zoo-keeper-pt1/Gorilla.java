48b324e76fcb5ce0c45fa3e72ef6861fda141aa8
Chris <cmsl4y3r@gmail.com>
2021-09-05 14:56:09 -0600

    added Mammal, Gorilla, and GorillaTest project completed

---
+++ 02-java-oop/03-zoo-keeper-pt1/Gorilla.java
@@ -0,0 +1,25 @@
+public class Gorilla extends Mammal {
+
+    // public void setEnergyLevel(int energy) {
+    //     this.energyLevel += energy;
+    // }
+
+
+    public void throwSomething(){
+        int energyReduce = -5;
+        System.out.println("The gorilla throws a rock at you! His energy is reduced by " + energyReduce*-1);
+        changeEnergyLevel(energyReduce);
+    }
+
+    public void eatBananas() {
+        int energyChange = 10;
+        System.out.println("The gorilla eats a banana, and looks satisfied. He gains " + energyChange + " energy.");
+        changeEnergyLevel(energyChange);
+    }
+
+    public void climb() {
+        int energyChange = -10;
+        System.out.println("The gorilla starts climbing the walls.  He expends " + energyChange*-1 + " energy.");
+        changeEnergyLevel(energyChange);
+    }
+}
