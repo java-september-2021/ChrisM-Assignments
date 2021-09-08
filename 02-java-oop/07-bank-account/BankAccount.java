ca57bc6c914af1c718e6e99b95bdd27b39b44185
Chris <cmsl4y3r@gmail.com>
2021-09-08 09:34:05 -0600

    BankAccount and BankAccountTest Complete, including random 10 digit account numbers

---
+++ 02-java-oop/07-bank-account/BankAccount.java
@@ -0,0 +1,81 @@
+import java.util.Random;
+
+public class BankAccount {
+    private double checkingBalance = 0.00;
+    private double savingsBalance = 0.00;
+    private static int numberOfAccounts = 0;
+    private static double totalMoneyStored = 0.00;
+    private Long accountNumber = 0L;
+
+    public BankAccount() {
+        this.checkingBalance = 0.00;
+        this.savingsBalance = 0.00;
+        numberOfAccounts++;
+        totalMoneyStored += this.checkingBalance + this.savingsBalance;
+        this.accountNumber = generateAccount(10);
+    }
+
+    public String numberGenerator() {
+        Random random = new Random();
+        Integer number = random.nextInt(10);
+        return number.toString();
+    }
+
+    public Long generateAccount(int length) {
+        String account = "";
+        for (int i = 0; i < length; i++) {
+            account = account + numberGenerator();
+        }
+
+        return Long.parseLong(account);
+    }
+
+    public Long getAccount() {
+        return this.accountNumber;
+    }
+
+    public double getCheckingBalance() {
+        return checkingBalance;
+    }
+
+    public double getSavingsBalance() {
+        return savingsBalance;
+    }
+
+    public void deposit(boolean checking, double amount) {
+        if (checking == true) {
+            this.checkingBalance += amount;
+            System.out.println("$" + amount + " deposited into checking.  Balance now at $" + this.checkingBalance);
+        } else {
+            this.savingsBalance += amount;
+            System.out.println("$" + amount + " deposited into savings.  Balance now at $" + this.savingsBalance);
+        }
+    }
+
+    public void withdraw(boolean checking, double amount) {
+        if (checking == true) {
+            if (amount < this.checkingBalance) {
+                this.checkingBalance -= amount;
+                System.out.printf("$ %.2f Withdrawn from checking.  Balance now at $ %.2f \n", amount,
+                        this.checkingBalance);
+            } else {
+                System.out.printf("Not enough in account.  Balance is at %.2f \n", checkingBalance);
+
+            }
+        } else {
+            if (amount < this.savingsBalance) {
+                this.savingsBalance -= amount;
+                System.out.printf("$%.2f withdrawn from savings.  Balance now at $%.2f \n", amount,
+                        this.savingsBalance);
+            } else {
+                System.out.printf("Not enough in account.  Balance is at $%.2f \n", savingsBalance);
+            }
+        }
+    }
+
+    public void viewTotalAccountBalance() {
+        double totalBalance = this.checkingBalance + this.savingsBalance;
+        System.out.printf("The combined total balance from your savings and checking accounts is $%.2f \n",
+                totalBalance);
+    }
+}
---
+++ 02-java-oop/07-bank-account/BankAccountTest.java
@@ -0,0 +1,18 @@
+public class BankAccountTest {
+    public static void main(String[] args) {
+        BankAccount myAccount = new BankAccount();
+        BankAccount yourAccount = new BankAccount();
+
+        myAccount.deposit(true, 200.99);
+        myAccount.withdraw(true, 400);
+        myAccount.withdraw(true, 20.55);
+        myAccount.deposit(false, 600.34);
+        myAccount.withdraw(false, 1000);
+        myAccount.withdraw(false, 44.44);
+        myAccount.viewTotalAccountBalance();
+
+        System.out.println(myAccount.getAccount());
+        System.out.println(myAccount.getAccount());
+        System.out.println(yourAccount.getAccount());
+    }
+}
